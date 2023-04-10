a247ec2de825a32a27f4314e600015fc75127dd8
HartwigH <hartwig.hii@gmail.com>
2023-04-10 13:34:59 +0300

    chore(initial): setups

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import "./App.scss";
+import "./style/App.scss";
+import "./style/weather-icons/sass/weather-icons.min.scss";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello world!</h1>
+      <h1 className="title">Hello world!</h1>
+      <i class="wi wi-day-sunny wi-flip-vertical"></i>
     </div>
   );
 }
