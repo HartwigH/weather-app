100c26257b47a70365a52a4d297ec87fb6ef134b
HartwigH <hartwig.hii@gmail.com>
2023-04-10 14:38:12 +0300

    style(initial): add responsive stuff

--- src/App.js
+++ src/App.js
@@ -3,9 +3,11 @@ import "./style/weather-icons/sass/weather-icons.min.scss";
 
 function App() {
   return (
-    <div className="App">
-      <h1 className="title">Hello world!</h1>
-      <i class="wi wi-day-sunny wi-flip-vertical"></i>
+    <div className="app">
+      <div className="container">
+        <h1 className="title">Hello world!</h1>
+        <i class="wi wi-day-sunny wi-flip-vertical"></i>
+      </div>
     </div>
   );
 }
